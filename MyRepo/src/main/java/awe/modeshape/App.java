3671998946334485729f1fe0fc7156d3a1f31a9e
Anders Weijnitz <anders.weijnitz@gmail.com>
2012-10-17 22:51:01 +0200

    First working version with repository deployment

--- MyRepo/src/main/java/awe/modeshape/App.java
+++ MyRepo/src/main/java/awe/modeshape/App.java
@@ -1,13 +1,73 @@
 package awe.modeshape;
 
+import org.modeshape.jcr.ModeShapeEngine;
+import org.modeshape.jcr.RepositoryConfiguration;
+import org.modeshape.common.collection.Problems;
+
+import javax.jcr.Repository;
+import javax.jcr.Session;
+import javax.jcr.Node;
+import javax.jcr.LoginException;
+import javax.jcr.RepositoryException;
+
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+	private Repository repository;
+
+	public App(Repository repository) {
+		this.repository = repository;
+
+	}
+
+	protected void printRootNode() 
+		throws LoginException, RepositoryException {
+		Session session = repository.login("default");
+		 
+		// Get the root node ...
+		Node root = session.getRootNode();
+		assert root != null;
+		 
+		System.out.println("Found the root node in the \"" + session.getWorkspace().getName() + "\" workspace");
+		 
+		session.logout();
+	}
+
+
+    public static void main( String[] args ) {
+    
+        System.out.println("Starting ModeShapeEngine");
+
+		// Create and start the engine ...
+		ModeShapeEngine engine = new ModeShapeEngine();
+		engine.start();
+
+		if(args.length == 0) {
+			System.out.println("Usage MyRep <path to repository config file>");
+			return;
+		}
+		try {
+			RepositoryConfiguration config = RepositoryConfiguration.read(args[0]);
+
+			// Check config before deployment
+			Problems problems = config.validate();
+			if (problems.hasErrors()) {
+			    System.err.println("Problems starting the engine.");
+			    System.err.println(problems);
+			    return;
+			}
+
+			Repository repository = engine.deploy(config);
+
+			App repo = new App(repository);
+			repo.printRootNode();
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
     }
 }
